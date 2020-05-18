951d02791731f4cad79bb03d1a82faf61bab7aed
wojciechkubiak <wgkubiak@gmail.com>
2020-05-18 18:54:26 +0200

    Working table

--- src/containers/Protege.tsx
+++ src/containers/Protege.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Spinner, Modal, Button} from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react";
+import { Spinner, Modal, Button, Table } from "react-bootstrap";
 import Exam from "./../components/Forms/Exam";
 
-import 'core-js';
+import "core-js";
 
 interface Props {
-  userID: string
+  userID: string;
 }
 
 const Protege = (props: Props) => {
@@ -13,15 +13,16 @@ const Protege = (props: Props) => {
   const [examDateData, setExamDateData] = useState<any>([]);
   const token = localStorage.getItem("token");
   const [examDate, setExamDate] = useState("");
+  const [weeklyExamsData, setWeeklyExamsData] = useState<any>([]);
   const [equalDate, setEqualDate] = useState(false);
   const [modalShownAtSession, setModalShownAtSession] = useState(false);
-  
+
   const [modalShow, setModalShow] = useState(true);
 
   const hideModal = () => {
     setModalShow(false);
     sessionStorage.setItem("modal", "true");
-  }
+  };
 
   const getData = () => {
     console.log("Getting data");
@@ -29,44 +30,58 @@ const Protege = (props: Props) => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `${token}`
-      }
+        Authorization: `${token}`,
+      },
     })
-    .then(res => res.json())
-    .then(res => setExamDateData(res))
-    .catch(error => error);
-  }
+      .then((res) => res.json())
+      .then((res) => setExamDateData(res))
+      .catch((error) => error);
+  };
+
+  const getWeekData = () => {
+    console.log("Getting data");
+    fetch(`https://opqn-api.herokuapp.com/exams`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setWeeklyExamsData(res))
+      .catch((error) => error);
+  };
 
   useEffect(() => {
     const isShown = sessionStorage.getItem("modal");
-    if(isShown) {
+    if (isShown) {
       setModalShownAtSession(true);
     }
-  })
+  });
 
   useEffect(() => {
     getData();
+    getWeekData();
   }, [equalDate, loaded]);
 
   useEffect(() => {
-    if(examDateData) {
-      examDateData.map(element => {
+    if (examDateData) {
+      examDateData.map((element) => {
         setExamDate(element.date);
-      })
+      });
     }
 
-    if(examDate) {
+    if (examDate) {
       const sameDate = compareDates(examDate);
       sameDate === 0 ? setEqualDate(true) : setEqualDate(false);
       setLoaded(true);
     }
 
+    console.log(weeklyExamsData);
+    console.log(examDateData);
+  }, [examDateData, examDate, equalDate, weeklyExamsData]);
 
-  }, [examDateData, examDate, equalDate])
-
-  
-
-  const compareDates = (date : string) => {
+  const compareDates = (date: string) => {
     const _date = new Date();
     const exam = new Date(date);
 
@@ -75,83 +90,150 @@ const Protege = (props: Props) => {
 
     console.log(res, _res);
     return res.localeCompare(_res);
-  }
+  };
 
-  const splitDate = (date : Date) => {
+  const splitDate = (date: Date) => {
     const d = date.getDate();
     const m = date.getMonth() + 1;
     const y = date.getFullYear();
 
     return `${d}/${m}/${y}`;
-  }
+  };
 
   return (
-      <div className="patron">
-        {loaded && (
-            <div className="patron--container">
-              {
-                !equalDate && (
-                        <Exam loadedHandler={setLoaded}/>
-                      )
-              }
-              {
-                equalDate && (
-                    <div className="exam--send-info">
-                      <div style={{width: "100%", height: "20em", position: "relative"}}>
-                        <h3 style={{color: "white", fontSize: "48px", textAlign: "center", position: "absolute", left: "50%", top: "50%", fontWeight: "bold", transform: "translate(-50%, -50%)"}}>TO JUŻ WSZYSTKO NA DZISIAJ.<br/>WRÓĆ JUTRO!</h3>
-                      </div>
-                    </div>
-                )
-              }
+    <div className="patron">
+      {loaded && (
+        <div className="patron--container">
+          {!equalDate && <Exam loadedHandler={setLoaded} />}
+          {equalDate && (
+            <div className="exam--send-info">
+              <div
+                style={{ width: "100%", height: "20em", position: "relative" }}
+              >
+                <h3
+                  style={{
+                    color: "white",
+                    fontSize: "48px",
+                    textAlign: "center",
+                    position: "absolute",
+                    left: "50%",
+                    top: "50%",
+                    fontWeight: "bold",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                >
+                  TO JUŻ WSZYSTKO NA DZISIAJ.
+                  <br />
+                  WRÓĆ JUTRO!
+                </h3>
+              </div>
             </div>
-        )}
-        {!loaded && (
-            <Button
-            variant="success"
-            style={{
-              position: "relative",
-              left: "50%",
-              top: "50%",
-              transform: "translate(-50%, -50%)",
-              borderRadius: "0"
-            }}
-            disabled
-          >
-            <Spinner
-              as="span"
-              animation="grow"
-              size="sm"
-              role="status"
-              aria-hidden="true"
-            />
-            Loading...
-          </Button>
-        )}
-        {!modalShownAtSession && (
-            <Modal
-                show={modalShow}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
+          )}
+          {weeklyExamsData && (
+            <Table
+              bordered
+              hover
+              variant="secondary"
+              style={{
+                position: "relative",
+                width: "80%",
+                left: "10%",
+                margin: "2em",
+              }}
             >
-              <Modal.Header style={{backgroundColor: "#292930", borderRadius: "0"}}>
-                <Modal.Title id="contained-modal-title-vcenter" style={{color: "rgba(255, 255, 255, 0.8)"}}>
-                  Informacja
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body style={{borderRadius: "0"}}>
-                <p style={{color: "rgba(0,0,0,0.67)"}}>
-                  Upewnij się, że wszystkie wprowadzone dane są zgodne ze stanem faktycznym.<br/>
-                  Pamiętaj również, aby nie manipulować otrzymanymi danymi. Twoje liczby nikogo nie zszokują.
-                </p>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" style={{borderRadius: "0"}} onClick={hideModal}>Wyjdź</Button>
-                <Button variant="success" style={{borderRadius: "0"}} onClick={hideModal}>Rozumiem</Button>
-              </Modal.Footer>
-            </Modal>
-        )}
-      </div>
+              <thead>
+                <tr style={{backgroundColor: "#5cb85c", color: "rgba(255, 255, 255, 1)", textAlign: "center"}}>
+                  <th>Data</th>
+                  <th>Waga</th>
+                  <th>Glukoza</th>
+                  <th>Ciśnienie</th>
+                </tr>
+              </thead>
+              <tbody>
+                  {
+                    weeklyExamsData.map(data => {
+                      return (
+                        <tr onClick={() => alert(data.id)} style={{backgroundColor: "#ffffff", textAlign: "center", color: "rgba(0, 0, 0, 0.6)"}}>
+                          <td style={{width: "25%"}}>{data.date.substring(0, 10)}</td>
+                          <td style={{width: "25%"}}>{data.weight}</td>
+                          <td style={{width: "25%"}}>{data.glucose}</td>
+                          <td style={{width: "25%"}}>{data.pressure}</td>
+                        </tr>
+                      )
+                    })
+                  }
+              </tbody>
+            </Table>
+          )}
+        </div>
+      )}
+      {!loaded && (
+        <Button
+          variant="success"
+          style={{
+            position: "relative",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "0",
+          }}
+          disabled
+        >
+          <Spinner
+            as="span"
+            animation="grow"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          Loading...
+        </Button>
+      )}
+      {!modalShownAtSession && (
+        <Modal
+          show={modalShow}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header
+            style={{ backgroundColor: "#292930", borderRadius: "0" }}
+          >
+            <Modal.Title
+              id="contained-modal-title-vcenter"
+              style={{ color: "rgba(255, 255, 255, 0.8)" }}
+            >
+              Informacja
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={{ borderRadius: "0" }}>
+            <p style={{ color: "rgba(0,0,0,0.67)" }}>
+              Upewnij się, że wszystkie wprowadzone dane są zgodne ze stanem
+              faktycznym.
+              <br />
+              Pamiętaj również, aby nie manipulować otrzymanymi danymi. Twoje
+              liczby nikogo nie zszokują.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              style={{ borderRadius: "0" }}
+              onClick={hideModal}
+            >
+              Wyjdź
+            </Button>
+            <Button
+              variant="success"
+              style={{ borderRadius: "0" }}
+              onClick={hideModal}
+            >
+              Rozumiem
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
   );
 };
 
